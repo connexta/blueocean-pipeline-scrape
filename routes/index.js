d42b2a5d2ff0866b24d07e4926c3586b205bd9a1
Matthew Bates <matthew.bates@connexta.com>
2018-10-31 06:02:43 +0000

    Fixed scraper crashing, updated npm packages, added automatic dynamic css library downloads

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,32 @@ router.get('/pipeline', (req, res) => {
 
     const url = `${host}/service/jenkins/blue/organizations/jenkins/${project}/detail/${branch}/${id}/pipeline`;
 
+    // download the current css libraries
+    var jenkins_css = ["service/jenkins/adjuncts/5a4bffce/io/jenkins/blueocean/blueocean-core-js.css", "service/jenkins/adjuncts/5a4bffce/io/jenkins/blueocean/blueocean.css", "service/jenkins/adjuncts/5a4bffce/io/jenkins/blueocean/jenkins-design-language.css"];
+    var wget = require('node-wget');
+
+    function downloadCSS(css) {
+        wget({
+                url:  `${host}/${css}`,
+                dest: './public/stylesheets/',      // destination path or path with filenname, default is ./
+                timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds
+            },
+            function (error, response, body) {
+                if (error) {
+                    console.log('--- error:');
+                    console.log(error);            // error encountered
+                } else {
+                    console.log('--- headers:');
+                    console.log(response.headers); // response headers
+                    console.log('--- body:');
+                    console.log(body);             // content of package
+                }
+            }
+        );
+    }
+
+    jenkins_css.forEach(downloadCSS);
+
     const nightmare = Nightmare({
         switches: {
             'ignore-certificate-errors': true
